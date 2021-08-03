c7b0b506326e6fa0e4e01860b9b888b19316c1a6
yogach <471534369@qq.com>
2021-08-03 14:26:14 +0800

    完成登录对话框的实验

--- QLoginDialog/QLoginDialog.h
+++ QLoginDialog/QLoginDialog.h
@@ -5,6 +5,7 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QLineEdit>
+#include <QTimer>
 
 class QLoginDialog : public QDialog
 {
@@ -12,15 +13,27 @@ class QLoginDialog : public QDialog
 private:
     QLabel UserLabel;
     QLabel PwdLabel;
+    QLabel CaptLabel;
     QLineEdit UserEdit;
     QLineEdit PwdEdit;
+    QLineEdit CaptEdit;
     QPushButton LoginBtn;
     QPushButton CancelBtn;
     QString m_User;
     QString m_Pwd;
+    QString m_captcha;
+    Qt::GlobalColor* m_colors;
+    QTimer m_timer;
+
+protected:
+    void paintEvent(QPaintEvent *);
+    QString getCaptcha();
+    Qt::GlobalColor* getColor();
+
 protected slots:
     void LoginBtn_Clicked();
     void CancelBtn_Clicked();
+    void onTimerOut();
 public:
     QLoginDialog(QWidget *parent = 0);
     QString getUser();
--- QLoginDialog/moc_QLoginDialog.cpp
+++ QLoginDialog/moc_QLoginDialog.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'QLoginDialog.h'
 **
-** Created: Fri Apr 9 11:08:26 2021
+** Created: Tue Aug 3 13:57:54 2021
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,7 +23,7 @@ static const uint qt_meta_data_QLoginDialog[] = {
        5,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -33,13 +33,14 @@ static const uint qt_meta_data_QLoginDialog[] = {
  // slots: signature, parameters, type, tag, flags
       14,   13,   13,   13, 0x09,
       33,   13,   13,   13, 0x09,
+      53,   13,   13,   13, 0x09,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_QLoginDialog[] = {
     "QLoginDialog\0\0LoginBtn_Clicked()\0"
-    "CancelBtn_Clicked()\0"
+    "CancelBtn_Clicked()\0onTimerOut()\0"
 };
 
 const QMetaObject QLoginDialog::staticMetaObject = {
@@ -73,9 +74,10 @@ int QLoginDialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         switch (_id) {
         case 0: LoginBtn_Clicked(); break;
         case 1: CancelBtn_Clicked(); break;
+        case 2: onTimerOut(); break;
         default: ;
         }
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
